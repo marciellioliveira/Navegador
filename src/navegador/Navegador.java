da51d44c7182524e1d473a194da286d51cd67773
Marcielli Oliveira <marciellileticiaol@gmail.com>
2016-07-05 22:28:40 -0300

    Inseri Painel com guias para parecer a pesquisa de um navegador Botões de Back, Next, Refresh e Home. (Ainda sem ícone)

--- src/navegador/Navegador.java
+++ src/navegador/Navegador.java
@@ -5,6 +5,7 @@
  */
 package navegador;
 
+import java.beans.PropertyVetoException;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
@@ -15,15 +16,12 @@ import javax.swing.JTextField;
 public class Navegador extends javax.swing.JFrame {
 
     /**
-     * Creates new form Navegador
+     * Creates new form Teste
      */
     public Navegador() {
-        initComponents();     
-
+        initComponents();
         setExtendedState(MAXIMIZED_BOTH);
-        
     }
-    
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -34,176 +32,182 @@ public class Navegador extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jDesktopPane1 = new javax.swing.JDesktopPane();
-        jPanel1 = new javax.swing.JPanel();
-        jSeparator1 = new javax.swing.JSeparator();
+        jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanelBG = new javax.swing.JPanel();
+        jTabbedPane2 = new javax.swing.JTabbedPane();
+        jPanelBGAcimaDasAbas = new javax.swing.JPanel();
+        jDesktopPaneBG = new javax.swing.JDesktopPane();
+        jButtonVoltar = new javax.swing.JButton();
+        jButtonAvancar = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
         jTextFieldPesquisa = new javax.swing.JTextField();
         jButtonEnter = new javax.swing.JButton();
-        jPanelBG = new javax.swing.JPanel();
-        jDesktopPaneBG = new javax.swing.JDesktopPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTextFieldPesquisa.setText("www.tecgirl.com.br");
+        javax.swing.GroupLayout jDesktopPaneBGLayout = new javax.swing.GroupLayout(jDesktopPaneBG);
+        jDesktopPaneBG.setLayout(jDesktopPaneBGLayout);
+        jDesktopPaneBGLayout.setHorizontalGroup(
+            jDesktopPaneBGLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 841, Short.MAX_VALUE)
+        );
+        jDesktopPaneBGLayout.setVerticalGroup(
+            jDesktopPaneBGLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 420, Short.MAX_VALUE)
+        );
+
+        jButtonVoltar.setText("<-");
+
+        jButtonAvancar.setText("->");
+
+        jButton2.setText("@");
+
+        jButton3.setText("^");
+
+        jTextFieldPesquisa.setText("www.facebook.com.br");
         jTextFieldPesquisa.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jTextFieldPesquisaActionPerformed(evt);
             }
         });
 
-        jButtonEnter.setText("Enter");
-        jButtonEnter.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jButtonEnterMouseClicked(evt);
-            }
-        });
+        jButtonEnter.setText(";)");
         jButtonEnter.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButtonEnterActionPerformed(evt);
             }
         });
 
-        jDesktopPaneBG.addAncestorListener(new javax.swing.event.AncestorListener() {
-            public void ancestorMoved(javax.swing.event.AncestorEvent evt) {
-            }
-            public void ancestorAdded(javax.swing.event.AncestorEvent evt) {
-                jDesktopPaneBGAncestorAdded(evt);
-            }
-            public void ancestorRemoved(javax.swing.event.AncestorEvent evt) {
-            }
-        });
+        javax.swing.GroupLayout jPanelBGAcimaDasAbasLayout = new javax.swing.GroupLayout(jPanelBGAcimaDasAbas);
+        jPanelBGAcimaDasAbas.setLayout(jPanelBGAcimaDasAbasLayout);
+        jPanelBGAcimaDasAbasLayout.setHorizontalGroup(
+            jPanelBGAcimaDasAbasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(jDesktopPaneBG)
+            .addGroup(jPanelBGAcimaDasAbasLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jButtonVoltar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButtonAvancar)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jTextFieldPesquisa)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButtonEnter, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        jPanelBGAcimaDasAbasLayout.setVerticalGroup(
+            jPanelBGAcimaDasAbasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanelBGAcimaDasAbasLayout.createSequentialGroup()
+                .addGap(6, 6, 6)
+                .addGroup(jPanelBGAcimaDasAbasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButtonEnter)
+                    .addComponent(jButtonVoltar)
+                    .addComponent(jButtonAvancar)
+                    .addComponent(jButton2)
+                    .addComponent(jButton3)
+                    .addComponent(jTextFieldPesquisa))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jDesktopPaneBG))
+        );
 
-        javax.swing.GroupLayout jDesktopPaneBGLayout = new javax.swing.GroupLayout(jDesktopPaneBG);
-        jDesktopPaneBG.setLayout(jDesktopPaneBGLayout);
-        jDesktopPaneBGLayout.setHorizontalGroup(
-            jDesktopPaneBGLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
-        );
-        jDesktopPaneBGLayout.setVerticalGroup(
-            jDesktopPaneBGLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 450, Short.MAX_VALUE)
-        );
+        jTabbedPane2.addTab("tab1", jPanelBGAcimaDasAbas);
 
         javax.swing.GroupLayout jPanelBGLayout = new javax.swing.GroupLayout(jPanelBG);
         jPanelBG.setLayout(jPanelBGLayout);
         jPanelBGLayout.setHorizontalGroup(
             jPanelBGLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPaneBG)
+            .addComponent(jTabbedPane2)
         );
         jPanelBGLayout.setVerticalGroup(
             jPanelBGLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPaneBG)
-        );
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jSeparator1)
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(jTextFieldPesquisa, javax.swing.GroupLayout.DEFAULT_SIZE, 700, Short.MAX_VALUE)
-                        .addGap(18, 18, 18)
-                        .addComponent(jButtonEnter, javax.swing.GroupLayout.PREFERRED_SIZE, 93, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jPanelBG, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap())
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextFieldPesquisa, javax.swing.GroupLayout.PREFERRED_SIZE, 30, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButtonEnter, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jPanelBG, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
-        );
-
-        jDesktopPane1.setLayer(jPanel1, javax.swing.JLayeredPane.DEFAULT_LAYER);
-
-        javax.swing.GroupLayout jDesktopPane1Layout = new javax.swing.GroupLayout(jDesktopPane1);
-        jDesktopPane1.setLayout(jDesktopPane1Layout);
-        jDesktopPane1Layout.setHorizontalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        jDesktopPane1Layout.setVerticalGroup(
-            jDesktopPane1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jTabbedPane2)
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(jPanelBG, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jDesktopPane1)
+            .addComponent(jPanelBG, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         pack();
-        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jTextFieldPesquisaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldPesquisaActionPerformed
+        
+        jButtonEnterActionPerformed(evt);
+       
+    }//GEN-LAST:event_jTextFieldPesquisaActionPerformed
 
     private void jButtonEnterActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonEnterActionPerformed
         //tentar deixar fazer login com o ping tb
-        if((jTextFieldPesquisa.getText().equals("www.facebook.com.br")) || (jTextFieldPesquisa.getText().equals("facebook.com.br")) || (jTextFieldPesquisa.getText().equals("31.13.85.36"))) { 
-            
-            jTextFieldPesquisa.setText("www.facebook.com.br");
-            Login obj=new Login();
-            jDesktopPaneBG.add(obj);
-            obj.setVisible(true);
-            obj.setPosicao();
-            
-        } else if ((jTextFieldPesquisa.getText().equals("www.google.com.br")) || (jTextFieldPesquisa.getText().equals("google.com.br")) || (jTextFieldPesquisa.getText().equals("172.217.29.67"))) { //fazer || .com
-            
-            Google obj=new Google();
-            jDesktopPaneBG.add(obj);
-            obj.setVisible(true);
-            obj.setPosicao();
-            
-        } else if (jTextFieldPesquisa.getText().equals("www.tecgirl.com.br")) {
-            
-            TecGirl obj=new TecGirl();
-            jDesktopPaneBG.add(obj);
-            obj.setVisible(true);
-            obj.setPosicao();
         
+        if((jTextFieldPesquisa.getText().equals("www.facebook.com.br")) || (jTextFieldPesquisa.getText().equals("facebook.com.br")) || (jTextFieldPesquisa.getText().equals("31.13.85.36"))) {
+                
+            Facebook obj1=new Facebook();
+            jDesktopPaneBG.add(obj1);
+            try{
+                obj1.setMaximum(true);
+                obj1.setVisible(true);
+            obj1.setPosicao();
+            } catch (PropertyVetoException e) {
+                JOptionPane.showMessageDialog(rootPane, e);
+            }
+            
+
+        } else if ((jTextFieldPesquisa.getText().equals("www.google.com.br")) || (jTextFieldPesquisa.getText().equals("google.com.br")) || (jTextFieldPesquisa.getText().equals("172.217.29.67"))) { //fazer || .com
+
+            Google obj2=new Google();
+            jDesktopPaneBG.add(obj2);
+            try{
+                obj2.setMaximum(true);
+                obj2.setVisible(true);
+            obj2.setPosicao();
+            } catch (PropertyVetoException e) {
+                JOptionPane.showMessageDialog(rootPane, e);
+            }
+
+        } else if (jTextFieldPesquisa.getText().equals("www.tecgirl.com.br")) {
+
+            TecGirl obj3=new TecGirl();
+            jDesktopPaneBG.add(obj3);
+            try{
+                obj3.setMaximum(true);
+                obj3.setVisible(true);
+            obj3.setPosicao();
+            } catch (PropertyVetoException e) {
+                JOptionPane.showMessageDialog(rootPane, e);
+            }
+
         } else if (jTextFieldPesquisa.getText().equals("www.fateccruzeiro.edu.br")) {
-            
-            FatecCruzeiro obj=new FatecCruzeiro();
-            jDesktopPaneBG.add(obj);
-            obj.setVisible(true);
-            obj.setPosicao();
-            
-        } 
-       
-//        
-//        else {
-//            
-//            JOptionPane.showMessageDialog(rootPane, "Você errou o site. Digite www.facebook.com.br na barra de pesquisa");
-//            
-//        }
+
+            FatecCruzeiro obj4=new FatecCruzeiro();
+            jDesktopPaneBG.add(obj4);
+            try{
+                obj4.setMaximum(true);
+                obj4.setVisible(true);
+            obj4.setPosicao();
+            } catch (PropertyVetoException e) {
+                JOptionPane.showMessageDialog(rootPane, e);
+            }
+
+        }
+
+        //
+        //        else {
+            //
+            //            JOptionPane.showMessageDialog(rootPane, "Você errou o site. Digite www.facebook.com.br na barra de pesquisa");
+            //
+            //        }
     }//GEN-LAST:event_jButtonEnterActionPerformed
-
-    private void jButtonEnterMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButtonEnterMouseClicked
-
-    }//GEN-LAST:event_jButtonEnterMouseClicked
-
-    private void jTextFieldPesquisaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldPesquisaActionPerformed
-        jButtonEnterActionPerformed(evt);
-    }//GEN-LAST:event_jTextFieldPesquisaActionPerformed
-
-    private void jDesktopPaneBGAncestorAdded(javax.swing.event.AncestorEvent evt) {//GEN-FIRST:event_jDesktopPaneBGAncestorAdded
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jDesktopPaneBGAncestorAdded
 
     /**
      * @param args the command line arguments
@@ -231,6 +235,7 @@ public class Navegador extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(Navegador.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -241,16 +246,16 @@ public class Navegador extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButtonAvancar;
     private javax.swing.JButton jButtonEnter;
-    private javax.swing.JDesktopPane jDesktopPane1;
+    private javax.swing.JButton jButtonVoltar;
     private javax.swing.JDesktopPane jDesktopPaneBG;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanelBG;
-    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JPanel jPanelBGAcimaDasAbas;
+    private javax.swing.JTabbedPane jTabbedPane1;
+    private javax.swing.JTabbedPane jTabbedPane2;
     private javax.swing.JTextField jTextFieldPesquisa;
     // End of variables declaration//GEN-END:variables
-
-   
-
-  
 }
--- src/navegador/TecGirl.java
+++ src/navegador/TecGirl.java
@@ -169,21 +169,21 @@ public class TecGirl extends javax.swing.JInternalFrame {
         );
         jPanel6Layout.setVerticalGroup(
             jPanel6Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel8, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
         jPanel5.setLayout(jPanel5Layout);
         jPanel5Layout.setHorizontalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel6, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(jPanel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         jPanel5Layout.setVerticalGroup(
             jPanel5Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel5Layout.createSequentialGroup()
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addComponent(jPanel6, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(61, 61, 61))
+                .addContainerGap())
         );
 
         jPanel2.setBackground(new java.awt.Color(255, 51, 153));
@@ -256,8 +256,8 @@ public class TecGirl extends javax.swing.JInternalFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jPanel5, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 210, Short.MAX_VALUE)
+                .addComponent(jPanel5, javax.swing.GroupLayout.PREFERRED_SIZE, 48, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 216, Short.MAX_VALUE)
                 .addComponent(jPanel9, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
 
